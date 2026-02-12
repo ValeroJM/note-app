f8169ca072e3d5b14be2c044fd42c0441527106e
jmvalmeria <jmvalmeria@gmail.com>
2026-02-11 20:06:32 -0500

    Api layer terminada

--- js/NotesAPI.js
+++ js/NotesAPI.js
@@ -9,15 +9,26 @@ export default class NotesAPI {
 
     static saveNote(noteToSave){
         const notes = NotesAPI.getAllNotes();
-        
-        noteToSave.id = Math.floor(Math.random() * 1000000);
-        noteToSave.updated = new Date().toISOString();
-        notes.push(noteToSave);
+        const existing = notes.find(note => note.id == noteToSave.id);
 
+        //Edit/Update
+        if (existing){
+            existing.title = noteToSave.title;
+            existing.body = noteToSave.body;
+            existing.updated = new Date().toISOString();
+        } else{
+            noteToSave.id = Math.floor(Math.random() * 1000000);
+            noteToSave.updated = new Date().toISOString();
+            notes.push(noteToSave);
+        }
+        
         localStorage.setItem("notesapp-notes", JSON.stringify(notes));
     }
 
     static deleteNote(id){
+        const notes = NotesAPI.getAllNotes();
+        const newNotes = notes.filter(note => note.id != id);
 
+        localStorage.setItem("notesapp-notes", JSON.stringify(notes));
     }
 }
--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
 import NotesAPI from "./NotesAPI.js"
 
+
 NotesAPI.saveNote({
-    title:"New Note!",
-    body:"I am a new note."
+    //id:493979,
+    title: "The title has changed!",
+    body: "I am a new note."
 });
+
 
 console.log(NotesAPI.getAllNotes());
